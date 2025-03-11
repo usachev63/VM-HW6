dd33321f32e71cac4d62c58036ab8cd2b7e47441
Danila Usachev <duesna897@gmail.com>
2025-03-11 12:10:58 +0300

    snippet

--- new.cpp
+++ new.cpp
@@ -18,8 +18,6 @@ static void get_usage(struct rusage &usage) {
   }
 }
 
-#define PAGE_SIZE 4096
-
 struct Node {
   Node *next;
   unsigned node_id;
@@ -51,10 +49,13 @@ static void pool_sigsegv_handler(int, siginfo_t *info, void *ucontext) {
   }
 }
 
+static constexpr size_t PAGE_SIZE = 4096;
+static constexpr size_t round_up_to_whole_pages(size_t size) {
+  return (size + (PAGE_SIZE - 1)) & -PAGE_SIZE;
+}
+
 void MyPool::init(size_t size) {
-  pool_size = size;
-  if (auto rem = size % PAGE_SIZE)
-    pool_size += PAGE_SIZE - rem;
+  pool_size = round_up_to_whole_pages(size);
   base_ptr = mmap(nullptr, pool_size, PROT_READ | PROT_WRITE | PROT_GROWSDOWN,
                   MAP_PRIVATE | MAP_ANONYMOUS | MAP_GROWSDOWN, -1, 0);
   if (base_ptr == MAP_FAILED) {
